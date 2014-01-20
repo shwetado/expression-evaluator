80980cb0a1d97b059d58e63537787cc7b6ceb360
Shweta <shwetadongare28@gmail.com>
2014-01-20 16:17:18 +0530

    evaluator client accepts command line arguments

--- src/shwetado/cli/EvaluatorClient.java
+++ src/shwetado/cli/EvaluatorClient.java
@@ -4,7 +4,7 @@ import shwetado.evaluator.Evaluator;
 
 public class EvaluatorClient {
     public static void main(String[] args) {
-        Evaluator evaluator = new Evaluator("1 + 5");
+        Evaluator evaluator = new Evaluator(args[0]);
         System.out.println(evaluator.getExpression());
     }
 }
